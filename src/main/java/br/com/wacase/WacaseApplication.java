be864e0c46f6650544cec1433f18986ec48cf5db
jonatastonholo <rtjonatas@gmail.com>
2022-09-21 22:03:41 -0300

    initial commit

---
+++ src/main/java/br/com/wacase/WacaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.wacase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WacaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WacaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wacase/WacaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wacase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WacaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
